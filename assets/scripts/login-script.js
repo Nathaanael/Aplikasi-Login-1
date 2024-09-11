dbacb69df4da7e74b13f9d0302ebf239ad4cf857
Nathanael Chrystian <104243995+Nathaanael@users.noreply.github.com>
2024-09-11 10:25:32 +0700

    Update login-script.js
    menmbah tulisan baru

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -3,6 +3,7 @@
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ tambahan
  */
 
 const loginFormElement = document.querySelector('#loginForm');
